12567a265cd4cc58aba3381875f0d3fb0326a8e9
jonmills <jonmills@ea400d73-72ca-44e4-b263-ffb733a749d7>
2014-06-02 18:52:26 +0000

    enhanced kernel selection list with user input

--- kernel-list.sh
+++ kernel-list.sh
@@ -7,9 +7,10 @@ export PATH
 KERN_ARR=
 INIT_ARR=
 
-show_kern () {
+list_kernel () {
   KERNS=$(ls /boot/vmlin* | sort -r | cut -d '/' -f 3)
   INITS=$(ls /boot/init* | sort -r | cut -d '/' -f 3)
+
   num=1
   for i in $INITS
   do
@@ -24,16 +25,40 @@ show_kern () {
 	  KERN_ARR[${num}]=$i
 	  num=$((num+1))
   done;
+
+  if [ ${#KERN_ARR[@]} -ne ${#INIT_ARR[@]} ]; then
+	echo "WARNING: You do not have the same number of kernels & initrds;"
+	echo "This may not work..."
+  fi
 }
 
-
-
+select_kernel () {
 echo ""
 echo "Select an available kernel by number from below:"
-show_kern
-read ANSWER
+list_kernel
+read -t 30 ANSWER
 KERN="/boot/${KERN_ARR[${ANSWER}]}"
 INITRD="/boot/${INIT_ARR[${ANSWER}]}"
 
-echo "Your chosen kernel is $KERN"
-echo "Your chosen ramdisk is $INITRD"
+confirm_kernel
+}
+
+confirm_kernel() {
+echo "Your chosen kernel is $KERN.  Is this correct? (Y|n)"
+echo ""
+read -t 30 CONFIRM
+case "$CONFIRM" in
+	"y" | "Y" | "")
+		echo "Using kernel ${KERN_ARR[${ANSWER}]} and initrd ${INIT_ARR[${ANSWER}]}..."
+	;;
+
+        "n" | "N")
+		echo "Let's try again..."
+		select_kernel
+		
+	;;
+esac
+}
+
+
+select_kernel
