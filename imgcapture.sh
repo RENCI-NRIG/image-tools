5fbd6e0f482ff452739c642333f17c802e7e007a
Victor J. Orlikowski <vjo@duke.edu>
2017-09-28 12:50:45 -0400

    Clean up indentation. Also, ensure that the 64bit feature is disabled for ext4 FS creation - since ExoGENI racks are using too old of a version of the e2fsprogs to handle it.

--- imgcapture.sh
+++ imgcapture.sh
@@ -3,27 +3,29 @@
 # GENI PROJECT LICENSE
 # http://www.geni.net/wp-content/uploads/2009/02/geniprojlic.pdf
 #
-# Copyright (c) 2014 RENCI
+# Copyright (c) 2017 RENCI
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-# and/or hardware specification (the "Work"), to deal in the Work including the rights to use, copy,
-# modify, merge, publish, distribute, and sublicense, for non-commercial use, copies of the Work,
-# and to permit persons to whom the Work is furnished to do so, subject to the following
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and/or hardware specification
+# (the "Work"), to deal in the Work including the rights to use,
+# copy, modify, merge, publish, distribute, and sublicense, for
+# non-commercial use, copies of the Work, and to permit persons to
+# whom the Work is furnished to do so, subject to the following
 # conditions:
 #
-# The above copyright notice and this permission notice shall be included in all copies or
-# substantial portions of the Work.
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Work.
 #
-# The Work can only be used in connection with the execution of a project which is authorized by
-# the GENI Project Office (whether or not funded through the GENI Project Office).
-# THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
-# IN THE WORK.
+# The Work can only be used in connection with the execution of a
+# project which is authorized by the GENI Project Office (whether or
+# not funded through the GENI Project Office). THE WORK IS PROVIDED
+# "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+# OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE WORK
+# OR THE USE OR OTHER DEALINGS IN THE WORK.
 #
 
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin
@@ -37,58 +39,65 @@ KERN_ARR=  INIT_ARR=  KERN=  INITRD=
 url="http://www.your-own-webserver.edu/images"
 
 seek () {
-
-  case "$size" in
-    *T* | *TB*)
-        d=$(echo "$size" | awk -F'T' '{print $1}')
-        let "seek=($d << 40)/$blocksize"
-        ;;
-    *G* | *GB*)
-        d=$(echo "$size" | awk -F'G' '{print $1}')
-        let "seek=($d << 30)/$blocksize"
-        ;;
-    *M* | *MB*)
-        d=$(echo "$size" | awk -F'M' '{print $1}')
-        let "seek=($d << 20)/$blocksize"
-        ;;
-    *)
-	break
-	;;
-  esac
-  echo "$seek";
+    case "$size" in
+        *T* | *TB*)
+            d=$(echo "$size" | awk -F'T' '{print $1}')
+            let "seek=($d << 40)/$blocksize"
+            ;;
+        *G* | *GB*)
+            d=$(echo "$size" | awk -F'G' '{print $1}')
+            let "seek=($d << 30)/$blocksize"
+            ;;
+        *M* | *MB*)
+            d=$(echo "$size" | awk -F'M' '{print $1}')
+            let "seek=($d << 20)/$blocksize"
+            ;;
+        *)
+	    break
+	    ;;
+    esac
+    echo "$seek";
 }
 
 create () {
-  if type dd >/dev/null
-  then
-    dd if=/dev/zero of=$dest/filesystem bs=$blocksize seek=$seek count=0
-  else
-    exit 1
-  fi
+    if type dd >/dev/null
+    then
+        dd if=/dev/zero of=$dest/filesystem bs=$blocksize seek=$seek count=0
+    else
+        exit 1
+    fi
 }
 
 format () {
-  if type mkfs.ext4 >/dev/null
-  then
-    mkfs.ext4 -F -j $dest/filesystem >/dev/null
-  elif type mkfs.ext3 >/dev/null
-  then
-    mkfs.ext3 -F -j $dest/filesystem >/dev/null
-  elif type mkfs.ext2 >/dev/null
-  then
-    mkfs.ext2 -F -j $dest/filesystem >/dev/null
-  else
-    exit 1
-  fi
-
+    if type mkfs.ext4 >/dev/null
+    then
+        # Current ExoGENI racks have e2fsprogs that can't handle
+        # the "64bit" feature.
+        mkfs.ext4 -O ^64bit -F -j $dest/filesystem >/dev/null
+    elif type mkfs.ext3 >/dev/null
+    then
+        mkfs.ext3 -F -j $dest/filesystem >/dev/null
+    elif type mkfs.ext2 >/dev/null
+    then
+        mkfs.ext2 -F -j $dest/filesystem >/dev/null
+    else
+        exit 1
+    fi
 }
 
 copy () {
-cd /; find . ! -path "./tmp/*" ! -path "./proc/*" ! -path "./sys/*" \
-  ! -path "./mnt/*" ! -path "./${dest}/*" ! -path "./etc/ssh/ssh_host_*" \
-  ! -path "./var/lib/iscsi/*" ! -path "/root/.ssh/*" ! -path "./vagrant/*" \
-  ! -path "./etc/udev/rules.d/70-persistent-net.rules" ! -path "./home/vagrant/*" \
-  | cpio -pmdv ${dest}/mnt-image
+    cd /; find . ! -path "./tmp/*" \
+               ! -path "./proc/*" \
+               ! -path "./sys/*" \
+               ! -path "./mnt/*" \
+               ! -path "./${dest}/*" \
+               ! -path "./etc/ssh/ssh_host_*" \
+               ! -path "./var/lib/iscsi/*" \
+               ! -path "./root/.ssh/*" \
+               ! -path "./vagrant/*" \
+               ! -path "./etc/udev/rules.d/70-persistent-net.rules" \
+               ! -path "./home/vagrant/*" \
+        | cpio -pmdv ${dest}/mnt-image
 }
 
 
@@ -116,95 +125,91 @@ EOF
 
 
 list_kernel () {
-  KERNS=$(ls /boot/vmlin* | sort -r | cut -d '/' -f 3)
-  INITS=$(ls /boot/init* | sort -r | cut -d '/' -f 3)
+    KERNS=$(ls /boot/vmlin* | sort -r | cut -d '/' -f 3)
+    INITS=$(ls /boot/init* | sort -r | cut -d '/' -f 3)
 
-  num=1
-  for i in $INITS
-  do
-          INIT_ARR[${num}]=$i
-          num=$((num+1))
-  done;
+    num=1
+    for i in $INITS
+    do
+        INIT_ARR[${num}]=$i
+        num=$((num+1))
+    done;
 
-  num=1
-  for i in $KERNS
-  do
-          echo -e "\t$num)\t$i"
-          KERN_ARR[${num}]=$i
-          num=$((num+1))
-  done;
+    num=1
+    for i in $KERNS
+    do
+        echo -e "\t$num)\t$i"
+        KERN_ARR[${num}]=$i
+        num=$((num+1))
+    done;
 
-  if [ ${#KERN_ARR[@]} -ne ${#INIT_ARR[@]} ]; then
+    if [ ${#KERN_ARR[@]} -ne ${#INIT_ARR[@]} ]; then
         echo "WARNING: You do not have the same number of kernels & initrds;"
         echo "This may not work..."
-  fi
+    fi
 }
 
 select_kernel () {
+    echo ""
+    echo "Select an available kernel by number from below:"
+    list_kernel
+    read -t 15 ANSWER
 
-echo ""
-echo "Select an available kernel by number from below:"
-list_kernel
-read -t 15 ANSWER
+    # Choose newest kernel by default
+    [ -z $ANSWER ] && ANSWER=1
 
-# Choose newest kernel by default
-[ -z $ANSWER ] && ANSWER=1
+    KERN="${KERN_ARR[${ANSWER}]}"
+    INITRD="${INIT_ARR[${ANSWER}]}"
 
-KERN="${KERN_ARR[${ANSWER}]}"
-INITRD="${INIT_ARR[${ANSWER}]}"
-
-confirm_kernel
+    confirm_kernel
 }
 
 confirm_kernel () {
-echo "Your chosen kernel is $KERN.  Is this correct? (Y|n)"
-echo ""
-read -t 30 CONFIRM
-case "$CONFIRM" in
+    echo "Your chosen kernel is $KERN.  Is this correct? (Y|n)"
+    echo ""
+    read -t 30 CONFIRM
+    case "$CONFIRM" in
         "y" | "Y" | "")
-                echo "Using kernel ${KERN_ARR[${ANSWER}]} and initrd ${INIT_ARR[${ANSWER}]}..."
-        ;;
+            echo "Using kernel ${KERN_ARR[${ANSWER}]} and initrd ${INIT_ARR[${ANSWER}]}..."
+            ;;
 
         "n" | "N")
-                echo "Let's try again..."
-                select_kernel
-                
-        ;;
-esac
+            echo "Let's try again..."
+            select_kernel
+            ;;
+    esac
 }
 
 fix_fstab () {
-  if type mkfs.ext4 >/dev/null
-  then
-    echo  '/dev/vda / ext4 defaults 0 0' > ${dest}/mnt-image/etc/fstab
-  elif type mkfs.ext3 >/dev/null
-  then
-    echo  '/dev/vda / ext3 defaults 0 0' > ${dest}/mnt-image/etc/fstab
-  elif type mkfs.ext2 >/dev/null
-  then
-    echo  '/dev/vda / ext2 defaults 0 0' > ${dest}/mnt-image/etc/fstab
-  else
-    exit 1
-  fi
+    if type mkfs.ext4 >/dev/null
+    then
+        echo  '/dev/vda / ext4 defaults 0 0' > ${dest}/mnt-image/etc/fstab
+    elif type mkfs.ext3 >/dev/null
+    then
+        echo  '/dev/vda / ext3 defaults 0 0' > ${dest}/mnt-image/etc/fstab
+    elif type mkfs.ext2 >/dev/null
+    then
+        echo  '/dev/vda / ext2 defaults 0 0' > ${dest}/mnt-image/etc/fstab
+    else
+        exit 1
+    fi
 }
 
 finished () {
-
-type neuca-get-public-ip > /dev/null
-if [ -z $noneuca ] && [ $? eq 0 ]
-then
-  IP=$(neuca-get-public-ip)
-elif type curl >/dev/null
-then
-  IP=$(curl icanhazip.com)
-  RETVAL=$?
-  if [ $RETVAL -ne 0 ]; then
-    IP=$(curl ifconfig.me)
-  fi
-else
-  IP=`hostname`
-fi
-  
+    type neuca-get-public-ip > /dev/null
+    if [ -z $noneuca ] && [ $? eq 0 ]
+    then
+        IP=$(neuca-get-public-ip)
+    elif type curl >/dev/null
+    then
+        IP=$(curl icanhazip.com)
+        RETVAL=$?
+        if [ $RETVAL -ne 0 ]; then
+            IP=$(curl ifconfig.me)
+        fi
+    else
+        IP=`hostname`
+    fi
 
 cat << MSG
 The following files have been created:
@@ -215,11 +220,9 @@ $dest/${name}.xml
 
 MSG
 cat $dest/${name}.xml
-
 }
 
 meta () {
-
 cat > $dest/${name}.xml << EOF
 <images>
      <image>
@@ -242,20 +245,20 @@ EOF
 }
 
 usage () {
-	echo "Usage: $0 [-o (don't use neuca)] [-v (verbose)] [-n name] [-d destination] [-s size (M|MB|G|GB|T|TB)] [-u url (e.g. http://geni-images.renci.org/images)]" >&2
+    echo "Usage: $0 [-o (don't use neuca)] [-v (verbose)] [-n name] [-d destination] [-s size (M|MB|G|GB|T|TB)] [-u url (e.g. http://geni-images.renci.org/images)]" >&2
 }
 
 
 if [ $# -lt 2 ]; then
-  usage
-  exit 1
+    usage
+    exit 1
 fi
 
 
 # leading colon is so we do error handling
 while getopts :ovn:d:u:b:s: opt
 do
-        case $opt in
+    case $opt in
 	o)	noneuca="NONEUCA" ;;
         v)      set -x  ;;
         n)      name=$OPTARG    ;;
@@ -265,15 +268,15 @@ do
         s)      size=$OPTARG
 		seek=$(seek) 
 		if [ -z "$seek" ]; then
-			echo "Please specify size in units of M|MB|G|GB|T|TB"
-			exit 1
+		    echo "Please specify size in units of M|MB|G|GB|T|TB"
+		    exit 1
 		fi
 		;;
         *)      echo "$0: invalid option" >&2
 		usage
 		exit
 		;;
-        esac
+    esac
 done
 shift $((OPTIND - 1))   # Remove options, leave arguments
 
@@ -284,13 +287,13 @@ if [ ! -d $dest ]; then
 fi
 
 if [ -z "$name" ]; then
-  type neuca-get > /dev/null
-  if [ -z $noneuca ] && [ $? -eq 0 ]
-  then
-    name=$(neuca-get slice_name)
-  else
-    name="`hostname`"
-  fi
+    type neuca-get > /dev/null
+    if [ -z $noneuca ] && [ $? -eq 0 ]
+    then
+        name=$(neuca-get slice_name)
+    else
+        name="`hostname`"
+    fi
 fi
 
 select_kernel
